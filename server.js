49c48f194aca340f228c78f15f9da8811bdecf28
ARMYOT713 <flowercaroarmy@gmail.com>
2026-03-27 10:14:28 -0600

    Estructura limpia para Node y Vercel

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Conectado"))
+    .catch(err => console.log("Error:", err));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(` Servidor en puerto ${PORT}`));
